1c36159c2a89899030ec14a34b4d95c146d5fbfd
fbslo <admin@fbslo.net>
2018-07-06 18:22:46 +0200

    Update delegation
    Now script will update delegation instead of undelegating.

--- undelegate.js
+++ undelegate.js
@@ -4,10 +4,11 @@ var cron = require('node-cron'); //get CronJob --> npm install --save node-cron
 cron.schedule('* * * * *', function() { //use https://crontab.guru/ to set time to run this script.
             // * * * * * will run it every minute.
 
-        const ACCOUNT_NAME = 'your-account-name' //change account name
+       
+        const your_account = 'account_username' //change account name
+        const active_key = 'active_private_key' //change your active key
         const WALLET_FILTER = 'transfer'
-        steem.api.getAccountHistory(ACCOUNT_NAME, -1, 50, (err, result) => {
-          console.log(err,result)
+        steem.api.getAccountHistory(your_account, -1, 50, (err, result) => {
           let transfers = result.filter( tx => tx[1].op[0] === WALLET_FILTER )
           console.log(transfers)
           displayTransactions(transfers)
@@ -16,22 +17,27 @@ cron.schedule('* * * * *', function() { //use https://crontab.guru/ to set time 
           transactions.forEach((tx) => {
             let transfer = tx[1].op[1]
 
-            if(transfer.memo.includes("Please update your delegation:") && transfer.memo.includes("0.000000%20VESTS") && transfer.to == "your-account-name" && transfer.from == 'minnowbooster') {
-             //change your-account-name to your steem username in line 19 (above)
-              var str = transfer.memo;
+            if(transfer.memo.includes("Please update your delegation:") && transfer.to == your_account && transfer.from == 'minnowbooster') {
+             var str = transfer.memo;
              var res = str.split("=") && str.split("&");
              var delegatee123 = res[1].split("=");
              let delegatee1234 = delegatee123[1]
-             console.log('Undelegate to ' + delegatee123[1]);
+             console.log('Update delegation to @' + delegatee123[1] + "!");
 
-             var wif = 'your-active-private-key' //change your active key
-             var delegator = 'your-account-name' //change username
-             var vesting_shares = '0.000000 VESTS'
+             var vests = res[2].split("=");
+             var vests1 = vests[1].split("%")
+             console.log("Delegate " + vests1[0] + " vests!")
 
-             steem.broadcast.delegateVestingShares(wif, delegator, delegatee1234, vesting_shares, function(err, result) {
-          console.log('Undelegated to ' + delegatee123[1]);
+             let vesting_shares = vests1[0] + ' VESTS'
+
+             steem.broadcast.delegateVestingShares(active_key, your_account, delegatee1234, vesting_shares, function(err, result) {
+               if (err)
+                  console.log('Failure! ' + err);
+               else
+                  console.log('SUCCESS! Updated delegation of ' + vests1[0] + 'VESTS to @' + delegatee1234)
+
         });
-        };
-          });
-        }
+      };
+    });
+}
 })
